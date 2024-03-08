9fe88c053e118861bd8657ecbe89dfa7434f1473
eHarbaugh5 <129325140+eHarbaugh5@users.noreply.github.com>
2024-03-07 20:28:25 -0500

    First Batch of Assets
    Includes:
    Krishna Palacio's Tile map assets
    -Player sprite
    -frying pan sprite
    -Goblin sprite

---
+++ Compliments To the Death/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Windows;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D playerRB;
+    public float movementSpeed;
+    private float inputHorizontal;
+    private float inputVertical;
+    private Vector2 moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovementHandler();
+    }
+
+    void playerMovementHandler()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+        playerRotationHandler();
+        moveInput = new Vector2(inputHorizontal * movementSpeed, inputVertical * movementSpeed);
+        playerRB.AddForce(moveInput);
+    }
+
+    void playerRotationHandler()
+    {
+        if (inputVertical != 0 && inputHorizontal != 0)
+        {
+            //  Up + Right
+            if (inputVertical == 1f && inputHorizontal == 1)
+            {
+                playerRB.rotation = -45f;
+            }
+            //  Down + Right
+            else if (inputVertical == -1f && inputHorizontal == 1)
+            {
+                playerRB.rotation = -135f;
+            }
+            //  Down + left
+            else if (inputVertical == -1f && inputHorizontal == -1)
+            {
+                playerRB.rotation = 135f;
+            }
+            //  Up + Left
+            else if (inputVertical == 1f && inputHorizontal == -1)
+            {
+                playerRB.rotation = 45f;
+            }
+        }
+        else
+        {
+            //  Up
+            if (inputVertical == 1f && inputHorizontal == 0)
+            {
+                playerRB.rotation = 0f;
+            }
+            //  Down
+            else if (inputVertical == -1f && inputHorizontal == 0)
+            {
+                playerRB.rotation = 180f;
+            }
+            //  Left
+            else if (inputHorizontal == -1f && inputVertical == 0)
+            {
+                playerRB.rotation = 90f;
+            }
+            //  Right
+            else if (inputHorizontal == 1f && inputVertical == 0)
+            {
+                playerRB.rotation = -90f;
+            }
+        }
+        
+        
+        
+
+
+
+
+
+    }
+
+}
